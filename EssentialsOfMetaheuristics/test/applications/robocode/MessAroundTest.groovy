d6740288330bc0686f8f2795bb7dfc4e192f46ae
Riveascore <agaxx010@morris.umn.edu>
2013-04-11 20:03:48 -0500

    Added missing templates, sample tests that Nic gave us, and the robocode.jar file that is QUITE necessary to run things.

---
+++ EssentialsOfMetaheuristics/test/applications/robocode/MessAroundTest.groovy
@@ -0,0 +1,16 @@
+package applications.robocode
+
+import spock.lang.*
+
+class MessAroundTest extends Specification{
+	
+	def "Groovy string built in vars"(){
+		given:
+		//def command = "${userHome}/robocode/robocode.sh -battle "
+		
+		
+		
+		expect:
+		true
+	}
+}
